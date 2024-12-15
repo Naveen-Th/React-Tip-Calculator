22f9fdecc23c88c9d299e6241e5c115a0953ab44
Naveen-Th <gowdanavi279@gmail.com>
2024-12-15 13:22:24 +0530

    Updated

--- src/Tip.jsx
+++ src/Tip.jsx
@@ -1,45 +1,52 @@
 import React,{useState} from "react";
 
 export const Tip = () => {
-    const [input,setInput] =useState('');
+    const [input,setInput] = useState(0);
     const [percentage,setPercentage] = useState(0);
-    const [customRange,setCustomRange] = useState(0);
     const [people,setPeople] = useState(1);
+    const [submit,setSubmit] = useState();
 
     const handleInput = (e) =>{
-        setInput(e.target.value);
-    }
-
-    const handleClick = (e) => {
-        e.preventDefault()
-        setPercentage(customRange)
+        setInput(e.target.value >= 1 ? e.target.value : 1);
     }
 
     const handleRange = (e) => {
-        setCustomRange(e.target.value);
+        setPercentage(e.target.value);
     }
 
     const handlePeople = (e) => {
-        setPeople(e.target.value)
+        setPeople(people>=1 ? e.target.value : 1)
     }
+
+    var tip;
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+         tip = (Number(input) * (percentage/100)) / people ;
+        setSubmit(tip.toFixed(2));
+    }
+    
     return(
         <div className="container">
-            <section className="total-bill">
-                <h1>Tip Calculator</h1>
-                <p>Total bill amount</p>
-                <input type="text" placeholder="0.00" value={input} onChange={handleInput}/>
-            </section>
-            <section className="choose-tip">
-                <p>Choose Tip {customRange}% </p>
-                <input type="range" onChange={handleRange} />
-                <button onClick={handleClick}>Set</button>
-            </section>
-            <section className="people">
-                <input type="number" value={people} onChange={handlePeople}/>
-                <button>Calculate</button>
-            </section>
+            <form onSubmit={handleSubmit}>
+                <section className="total-bill">
+                    <h1>Tip <br></br> Calculator</h1>
+                    <p>Total bill amount</p>
+                    <input type="text" placeholder="0.00" value={input} onChange={handleInput}/> 
+                </section>
+                <section className="choose-tip">
+                    <p>Choose Tip {percentage}% </p>
+                    <input type="range" onChange={handleRange} />
+                </section>
+
+                <section className="people">
+                    <p>Number of people sharing</p>
+                    <input type="number" value={people} onChange={handlePeople}/> <br></br>
+                    <button className="btn" type="submit">Calculate</button>
+                </section>
+            </form>
             <div>
-                The Tip Per Person is ${Number(input)} {people}
+                <p>The Tip Per Person is ${submit} </p>
             </div>
         </div>
         
