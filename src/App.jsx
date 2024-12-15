bfe375600c5b16ea8b214bfbcc5a16708bafb952
Naveen-Th <gowdanavi279@gmail.com>
2024-12-15 13:18:41 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Tip } from './Tip';
 function App() {
 
   return (
-    <>
+    <div className='main'>
       <Tip></Tip>
-    </>
+    </div>
   )
 }
 
--- src/Tip.jsx
+++ src/Tip.jsx
@@ -1,96 +1,47 @@
 import React,{useState} from "react";
 
 export const Tip = () => {
-    const [input,setInput] = useState('');
-    const [percentage1,setPercentage1] = useState(0);
-    const [percentage2,setPercentage2] = useState(0);
-    const [select,setSelect] = useState(0);
-    const [isopen,setisOpen] = useState(false);
+    const [input,setInput] =useState('');
+    const [percentage,setPercentage] = useState(0);
+    const [customRange,setCustomRange] = useState(0);
+    const [people,setPeople] = useState(1);
 
-    const tip = Number(input * ((percentage1 + percentage2) / select / 100) );
-
-    const selected = () => {
-        setSelect((select)=>select+1)
-    }
-    
-    const onReset = () => {
-        setInput(0);
-        setPercentage1(0);
-        setPercentage2(0);
-        tip = 0;
+    const handleInput = (e) =>{
+        setInput(e.target.value);
     }
 
-    const open = () => {
-        setisOpen(!isopen)
+    const handleClick = (e) => {
+        e.preventDefault()
+        setPercentage(customRange)
     }
-    return(
-        <>
-           <BillInput input={input} setInput={setInput}></BillInput> 
 
-           <Select percentage={percentage1} setPercentage={setPercentage1} selected={selected}>
-            How did you like the service 
-            </Select>
+    const handleRange = (e) => {
+        setCustomRange(e.target.value);
+    }
 
-            <button onClick={open}>Friend</button>
-
-            { isopen &&
-            <Select percentage={percentage2} setPercentage={setPercentage2} selected={selected}>
-            How did your friend like the service 
-            </Select>
-            }
-            
-           <Output input={input} tip={tip}></Output>
-
-           <Reset onReset={onReset}></Reset>
-        </>
-    )
-}
-
-export const BillInput = ({input,setInput}) => {
-
-    const onChange = (e) => {
-        setInput(Number(e.target.value))
+    const handlePeople = (e) => {
+        setPeople(e.target.value)
     }
     return(
-        <>
-            <label>How much of a Tip  </label>
-            <input type="text"  onChange={onChange} value={input} placeholder="Enter a Price"/>
-        </>
-    )
-}
-
-
-export const Select = ({children,percentage,setPercentage,selected}) => {
-    
-    return(
-        <>
+        <div className="container">
+            <section className="total-bill">
+                <h1>Tip Calculator</h1>
+                <p>Total bill amount</p>
+                <input type="text" placeholder="0.00" value={input} onChange={handleInput}/>
+            </section>
+            <section className="choose-tip">
+                <p>Choose Tip {customRange}% </p>
+                <input type="range" onChange={handleRange} />
+                <button onClick={handleClick}>Set</button>
+            </section>
+            <section className="people">
+                <input type="number" value={people} onChange={handlePeople}/>
+                <button>Calculate</button>
+            </section>
             <div>
-            <label> { children } </label>
-                <select  onChange={e => setPercentage(Number(e.target.value))} value={percentage} onFocusCapture={selected}>
-                    <option value='0'>Dissatified 0$</option>
-                    <option value='5'>Average 5$</option>
-                    <option value='10'>Good 10$</option>
-                </select>
+                The Tip Per Person is ${Number(input)} {people}
             </div>
-        </>
+        </div>
+        
     )
 }
-
-
-
-export const Output = ({tip}) => {
-    return(
-        <>
-            <h1>Tip : {Number(tip)}</h1>
-        </>
-    )
-}
-
-export const Reset = ({onReset}) => {
-    return(
-        <>
-        <button onClick={onReset}>Reset</button>
-        </>
-    )
-}
-
